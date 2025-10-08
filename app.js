ed5aefc39d37c2af388181be3f98e8869cb59e0b
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-08 20:09:33 +0530

    connect mongoose

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const PORT = 5000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// db connection
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect("mongodb://127.0.0.1:27017/first_app")
+      .then(() => console.log("DB Connected Successfully!"));
+  } catch (error) {}
+};
+connectDB();
+
 // Use routes
 app.use("/api/users", bookRoutes);
 
