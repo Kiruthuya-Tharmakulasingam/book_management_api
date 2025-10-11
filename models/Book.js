bf788a82e73608ac4fece336cb2a335c8e276d5a
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-11 18:51:49 +0530

    timestamps added

--- models/Book.js
+++ models/Book.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 
-const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: String,
+    publishedYear: Number,
+    price: Number,
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-  genre: String,
-  publishedYear: Number,
-  price: Number,
-  inStock: {
-    type: Boolean,
-    default: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Book", bookSchema, "books");
